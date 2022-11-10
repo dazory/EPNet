f0b9d3b9c08d5322e3353ce11c7afc7c517fea17
dazory <dazory@naver.com>
2022-11-10 15:00:03 +0900

    IMPORTANT CHANGE!! : make the order of dt_annos same as gt_annos

--- tools/kitti_object_eval_python/evaluate.py
+++ tools/kitti_object_eval_python/evaluate.py
@@ -17,10 +17,10 @@ def evaluate(label_path,
              current_class = 0,
              coco = False,
              score_thresh = -1):
-    dt_annos = kitti.get_label_annos(result_path)
+    val_image_ids = _read_imageset_file(label_split_file)
+    dt_annos = kitti.get_label_annos(result_path, val_image_ids)
     if score_thresh > 0:
         dt_annos = kitti.filter_annos_low_score(dt_annos, score_thresh)
-    val_image_ids = _read_imageset_file(label_split_file)
     gt_annos = kitti.get_label_annos(label_path, val_image_ids)
     if coco:
         return get_coco_eval_result(gt_annos, dt_annos, current_class)
